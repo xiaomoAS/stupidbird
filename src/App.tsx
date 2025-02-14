b20ca27c2f1499000b8eff6cc1b60f5025b7bd36
jiangzupei1 <jiangzupei1@jd.com>
2025-02-14 16:15:23 +0800

    react建仓

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+/*
+ * @Autor: jiangzupei
+ * @Description: 
+ * @Date: 2025-02-14 10:55:48
+ * @LastEditors: jiangzupei1 jiangzupei1@jd.com
+ * @LastEditTime: 2025-02-14 15:53:55
+ * @FilePath: /stupidbird/src/App.tsx
+ */
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
